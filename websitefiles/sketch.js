beee984782c4bf3dc211610f733b13246bb84f8a
ryanneross <ryannerjross@gmail.com>
2020-12-18 10:09:15 -0500

    so many ships!
    new ships and background change, lightspeed acheived! kinda; additional option background change for lightspeed?

--- websitefiles/sketch.js
+++ websitefiles/sketch.js
@@ -1,95 +1,83 @@
 let bg;
-// let img;
 var x = 0;
 var y = 0;
 var w = 175;
 var h = 102;
 let ships = [];
 var s = 0;
-// this.history = [];
 
 
 
 function setup() {
   // put setup code here
-  // push();
-  // noTint();
   bg = loadImage('assets/galaxy.jpg');
   createCanvas(1005, 750);
-  // pop();
   ships[0] = loadImage('assets/spaceship0.png');
   ships[1] = loadImage('assets/spaceship1.png');
-  ships[11] = loadImage('assets/spaceship10.png');
+  ships[2] = loadImage('assets/spaceship3.png');
+  ships[3] = loadImage('assets/spaceship4.png');
+  ships[4] = loadImage('assets/spaceship6.png');
   ships[10] = loadImage('assets/spaceship10.png');
+  ships[11] = loadImage('assets/spaceship11.png');
+  ships[12] = loadImage('assets/spaceship13.png');
+  ships[13] = loadImage('assets/spaceship14.png');
+  ships[14] = loadImage('assets/spaceship16.png');
 }
 
 function draw() {
   // put drawing code here
   push();
   noTint();
+    // if(keyIsDown(13)) {
+    // tint(0, 153, 204);
+    // }
   background(bg);
-  // image(img, x, y, w, h);
   pop();
-  // push();
-  // noTint();
 
   image(ships[s], x, y, w, h);
-  // pop();
 
   if ( x > width) {
-    x = 0;
+    x = -10;
   } else if (x < -100) {
     x = width;
   }
 
   if ( y > height) {
-    y = 0;
-  } else if (y < -100) {
+    y = -50;
+  } else if (y < -50) {
     y = height;
   }
 
   if(keyIsDown(LEFT_ARROW)) {
-    // scale left
-    // translate(width, 0);
-    // scale(-1,1.0);
     noTint();
-    if (s <= 1) {
+    if (s <= 4) {
       s = s + 10;
     }
-    // scale isn't working because it also changes position
     x -= 5;
   }
 
   if(keyIsDown(RIGHT_ARROW)) {
-    // scale right
-    // translate(width,0);
-    // scale(1.0,1.0);
-    // tint(0, 153, 204);
     noTint();
-    if (s > 1) {
+    if (s > 4) {
       s = s - 10;
     }
-    // scale isn't working because it also changes position
     x += 5;
   }
 
   if(keyIsDown(13)) {
-    // scale right
-    // translate(width,0);
-    // scale(1.0,1.0);
-    // push();
     tint(0, 153, 204);
-    // image(ships[s], x, y, w, h);
-    // scale isn't working because it also changes position
     x += 10;
     y -= 10;
     w -= 10;
     h -= 10;
+    if ( h < 10) {
+      h = 10;
+      w = 17;
+    }
 
-    image(ships[s], x-10, y+10, w+10, h+10);
+    // image(ships[s], x-10, y+10, w+10, h+10);
 
     // image(ships[s], x-5, y+5, w+5, h+5);
-    // pop();
   }
   
   if(keyIsDown(UP_ARROW)) {
@@ -103,6 +91,7 @@ function draw() {
   }
 
   if (keyIsDown(67)) {
+    noTint();
     w += 10;
     h += 10;
     //add max
@@ -111,8 +100,13 @@ function draw() {
   }
 
   if (keyIsDown(70)) {
+    noTint();
     w -= 10;
     h -= 10;
+    if ( h < 10) {
+      h = 10;
+      w = 17;
+    }
     //add min
   
     // f makes objects "further" or smaller
@@ -124,28 +118,11 @@ function draw() {
 
 function keyPressed() {
   if (keyCode === (32)) {
+    noTint();
     s++;
-    if (s > 1) {
+    if (s > 4) {
       s = 0;
     } 
   }
 
-  // push();
-  // if (keyCode === (13)) {
-  //   tint(0, 153, 204);
-  //   image(ships[s], x, y, w, h);
-  //   x += 50;
-  //   y -= 50;
-  //   image(ships[s], x, y, w, h);
-  //   tint(0, 153, 204);
-  //   // frameRate(10);
-  //   tint(0, 153, 204);
-  //   // var v= createVector (this.x, this.y);
-  //   // this.history.push(v);
-  //   // for (var i = 0; i < this.history.length; i++){
-  //   //   var loc = this.history[i];
-  //   //   ellipse (loc.x, loc.y, 15, 15);
-  //   // }
-  // }
-  // pop();
 }
